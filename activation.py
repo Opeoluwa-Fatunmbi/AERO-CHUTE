0216691e540abc7be04078861bb91c6ade3c2803
Opeoluwa Fatunmbi <101920613+opeoluwaaaa@users.noreply.github.com>
2022-10-13 12:23:53 +0100

    Add files via upload

---
+++ activation.py
@@ -0,0 +1,6 @@
+import screamDetector
+
+
+class Activation:
+    def __init__(self) -> None:
+        pass
---
+++ screamDetector.py
@@ -0,0 +1,16 @@
+'''
+It is a natural instinct to scream and panick when there is a sudden emergency on a plane
+This feature is to monitor the scream level of the passengers on board
+'''
+
+class screamDetector:
+    def __init__(self,sound) -> None:
+        self.sound = sound
+
+
+    def scream_level(init):
+        pass
+
+
+
+    
